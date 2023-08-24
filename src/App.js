de72a54829658b66648d5511c4894f42ace46287
masa-massara <masaya.yoshikawa.31@icloud.com>
2023-08-24 17:28:02 +0900

    「pullできてる」を追加

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>テストバージョンpullできてる！</p>
+        <p>pullできてる！</p>
         <a
           className="App-link"
           href="https://reactjs.org"
