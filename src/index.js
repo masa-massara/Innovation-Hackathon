cdfed94f068a3ef04db9cb49d107580f87b3c395
masa-massara <masaya.yoshikawa.31@icloud.com>
2023-09-10 11:21:47 +0900

    firebaseの読み取りを追加

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import LanguageSelection from './select_language'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <LanguageSelection />
   </React.StrictMode>
 );
 
